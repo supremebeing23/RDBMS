b33acebd59902ac3537b2c0d1ee34251c7a1246d
sb <sb@ubuntu.myguest.virtualbox.org>
2023-11-09 10:28:24 +0200

    first commit

---
+++ mongo-labs/src/main/java/mongodb/mongolabs/MongoLabsApplication.java
@@ -0,0 +1,13 @@
+package mongodb.mongolabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoLabsApplication.class, args);
+	}
+
+}
---
+++ mongo-labs/src/test/java/mongodb/mongolabs/MongoLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package mongodb.mongolabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
